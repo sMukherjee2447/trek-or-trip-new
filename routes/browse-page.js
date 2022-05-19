3e49310bef1ec1f207162a16e1fbe85cfe5b3935
Subham Mukherjee <smukherjee2447@gmail.com>
2022-05-19 14:29:24 +0530

    browse functionality done

--- routes/browse-page.js
+++ routes/browse-page.js
@@ -1,9 +1,30 @@
 const express = require('express')
 
 const router = express.Router()
+const MongoClient = require('mongodb').MongoClient
+
+var database
+
+const connection = MongoClient.connect('mongodb+srv://subham:subham@cluster0.ojwma.mongodb.net/test', {
+    useNewUrlParser: true
+}, (error, result) => {
+    if (error)
+        throw error
+    database = result.db('trek-or-trip')
+    console.log('Database Connected to browse-page.js')
+})
 
 router.get('/', (req, res) => {
-    res.render('browse')
+
+    const browse_data = database.collection('place').find({}).toArray((err, results_browse_Data) => {
+        if (err) {
+            throw err
+        }
+        res.render('browse', {
+            title: 'Browse all packages',
+            browse_data: results_browse_Data
+        })
+    })
 })
 
 module.exports = router
